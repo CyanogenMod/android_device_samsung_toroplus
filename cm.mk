aaab949d3067c22f6c4c7089335287ac246f2766
Robert Burns <burnsra@gmail.com>
2012-09-08 21:57:45 -0400

    Update build fingerprint to latest OTA (JRO03U)
    Change-Id: I90ecdd26b8596c1b1852ecccb221762eceb02a49

--- cm.mk
+++ cm.mk
@@ -23,4 +23,4 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.0.4/IMM76D/L700FD02:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.0.4 IMM76D L700FD02 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.1.1/JRO03U/L700FH05:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.1.1 JRO03U L700FH05 release-keys"
