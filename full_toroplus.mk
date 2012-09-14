c5f55438f0d59ba1155f38e329b7a57fdda7e993
Madan Ankapura <mankapur@sta.samsung.com>
2012-09-14 16:54:08 -0700

    add missing overlay files
    Bug:6940653
    Change-Id: Ic6cbbb17d8d69ab3e07f530bb0ad2062dc186aab
    Signed-off-by: Madan Ankapura <mankapur@sta.samsung.com>

--- full_toroplus.mk
+++ full_toroplus.mk
@@ -22,7 +22,7 @@
 #
 
 # Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES := device/samsung/toroplus/test-apns-conf_sprint.xml:system/etc/apns-conf.xml
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
