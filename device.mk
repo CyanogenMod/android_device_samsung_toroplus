e30e9041de43400086eb599b618519b93ff9dd06
Robert Burns <burnsra@gmail.com>
2012-12-02 20:57:03 -0500

    Remove need for CellBroadcastReceiver
    Change-Id: I4d76b8b2bb6597fb20525081ee7af7d54b18673e

--- device.mk
+++ device.mk
@@ -27,8 +27,5 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.vc_call_vol_steps=7
 
-PRODUCT_PACKAGES := \
-	CellBroadcastReceiver
-
 $(call inherit-product, device/samsung/tuna/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/toroplus/device-vendor.mk)
