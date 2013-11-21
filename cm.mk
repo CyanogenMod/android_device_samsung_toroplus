6d79cf0940e2448b1a2bdabf818c93ab08a78602
Michael Limb <something15525@gmail.com>
2013-11-20 22:46:12 -0800

    toroplus: update 4.3 fingerprint from L700GJ04 OTA
    Change-Id: I636d62f15e5edc3413be1134a31835c9db697b37

--- cm.mk
+++ cm.mk
@@ -23,4 +23,7 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 #Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysidspr BUILD_FINGERPRINT="google/mysidspr/toro:4.3/JWR66V/737497:user/release-keys" PRIVATE_BUILD_DESC="mysidspr-user 4.3 JWR66V 737497 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     PRODUCT_NAME=mysidspr \
+     BUILD_FINGERPRINT="samsung/mysidspr/toroplus:4.3/JWR67B/L700GJ04:user/release-keys" \
+     PRIVATE_BUILD_DESC="mysidspr-user 4.3 JWR67B L700GJ04 release-keys"
