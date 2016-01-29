135c73f571b35ba239359c89887b2d529c359ad5
Trulan Martin <trulanm@gmail.com>
2016-01-28 20:28:07 -0500

    toroplus: Add global symbol for libsec-ril_lte.so compatibility
    toroplus's RIL thinks it should be able to take screenshots.
    This 'feature' depends on a symbol that no longer exists. Add the
    missing symbol via a global static lib.
    
    Change-Id: I749051011cbe2a7ab194004dc4a195b101d6f456

---
+++ libsecril_global_symbols/libsecril-compatibility.c
@@ -0,0 +1,7 @@
+/**
+ * toroplus's RIL has the ability to take a screenshot. It is
+ * supposedly some vendor/testing/factory stuff. This codepath
+ * should never be hit under any legitimate circumstances.
+ * Just define the symbol.
+ */
+void _ZN7android16ScreenshotClient6updateEv() { }
