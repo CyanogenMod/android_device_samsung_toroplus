115527c4c07dc43ba37b620c9001edc968d2332e
Trulan Martin <trulanm@gmail.com>
2016-03-17 06:14:01 -0400

    toroplus: switch to mini phone config to save space
    Change-Id: I6f0bf406ee6e468074eb9044a60600c64603efe3

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/toroplus/aosp_toroplus.mk)
