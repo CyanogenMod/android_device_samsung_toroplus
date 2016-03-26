7338a3426bc56538ce2a7de45b4533ca6dd4dc98
Trulan Martin <trulanm@gmail.com>
2016-03-26 08:22:16 -0400

    Revert "toroplus: Add global symbol for libsec-ril_lte.so compatibility"
    This is now handled in libsecril-compat in the tuna device tree,
    and this concept would not work in cm13 anyway.
    
    This reverts commit 135c73f571b35ba239359c89887b2d529c359ad5.
    
    Change-Id: Ib5690261902056291111fd52e12b20a086a8ceab

--- libsecril_global_symbols/libsecril-compatibility.c
+++
@@ -1,7 +0,0 @@
-/**
- * toroplus's RIL has the ability to take a screenshot. It is
- * supposedly some vendor/testing/factory stuff. This codepath
- * should never be hit under any legitimate circumstances.
- * Just define the symbol.
- */
-void _ZN7android16ScreenshotClient6updateEv() { }
